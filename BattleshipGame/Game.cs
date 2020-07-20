d9ef5a0120af5c9528b9e4b2b3deda21263587e0
TimH <tmherron09@gmail.com>
2020-07-20 11:13:25 -0500

    Add left/right selection movement

--- BattleshipGame/Game.cs
+++ BattleshipGame/Game.cs
@@ -24,8 +24,8 @@ namespace BattleshipGame
 
         // Battlefield Display variables
         int topPad;
-        int leftPad;
-        int rightPad;
+        int leftPad;  // left and right padding must equal width - (playfield width * square size)
+        int rightPad;  
 
         // Selection Variables
         int selectionIndex;
@@ -42,9 +42,6 @@ namespace BattleshipGame
             this.height = height;
             buf = new byte[width * height];
             bufField = new CharInfo[width * height];
-            topPad = 0;
-            //leftPad = 25;
-            //rightPad = 25;
             playfieldHeight = 20;
             playfieldWidth = 20;
         }
@@ -61,15 +58,8 @@ namespace BattleshipGame
 
                     if (Console.KeyAvailable)
                     {
-
-
-
                         ConsoleKeyInfo readKey = Console.ReadKey(true);
-
-                        //Console.WriteLine($"{readKey.KeyChar}");
-                        //Thread.Sleep(200);
-                        
-                            ReadPlayerInput(readKey.Key);
+                        ReadPlayerInput(readKey.Key);                        
                         
                     }
 
@@ -88,66 +78,71 @@ namespace BattleshipGame
 
         private void ReadPlayerInput(ConsoleKey readKey)
         {
-            int displayX = 0;
-            int displayY = 0;
-            int playfieldX = 0;
-            int playfieldY = 0;
+            // Remove top padding and left padding to get index of 0,0 to calculate other x,y values.
+            int selectionZeroed = selectionIndex - ((topPad * width) + leftPad);
+            int displayX = selectionZeroed % (playfieldWidth * 2);
+            int displayY = (selectionZeroed / (width - leftPad)) % (width - leftPad);
+            int playfieldX = displayX / 2;
+            int playfieldY = displayY / 2;
             if (readKey == ConsoleKey.RightArrow)
             {
-                if (selectionIndex % 20 == 19)
+                if (playfieldX % 20 == 19)
                 {
-                    selectionIndex -= 19;
+                    selectionIndex -= (playfieldWidth * 2) -2;
                 }
                 else
                 {
-                    selectionIndex++;
+                    selectionIndex += 2; ;
                 }
             }
             else if (readKey == ConsoleKey.LeftArrow)
             {
-                if (selectionIndex % 20 == 0)
+                if (playfieldX % 20 == 0)
                 {
-                    selectionIndex += 19;
+                    selectionIndex += (playfieldWidth * 2) - 2;
                 }
                 else
                 {
-                    selectionIndex--;
+                    selectionIndex -= 2;
                 }
             }
-            else if (readKey == ConsoleKey.UpArrow)
-            {
-                if (selectionIndex <= 19)
-                {
-                    selectionIndex = bufField.Length - 20 + selectionIndex;
-                }
-                else
-                {
-                    selectionIndex -= 20;
-                }
-            }
-            else if (readKey == ConsoleKey.DownArrow)
-            {
-                if (selectionIndex >= bufField.Length - 20)
-                {
-                    selectionIndex = selectionIndex - bufField.Length + 20;
-                }
-                else
-                {
-                    selectionIndex += 20;
-                }
-            }
-            else if (readKey == ConsoleKey.Enter)
-            {
+            //else if (readKey == ConsoleKey.UpArrow)
+            //{
+            //    if (selectionIndex <= 19)
+            //    {
+            //        selectionIndex = bufField.Length - 20 + selectionIndex;
+            //    }
+            //    else
+            //    {
+            //        selectionIndex -= 20;
+            //    }
+            //}
+            //else if (readKey == ConsoleKey.DownArrow)
+            //{
+            //    if (selectionIndex >= bufField.Length - 20)
+            //    {
+            //        selectionIndex = selectionIndex - bufField.Length + 20;
+            //    }
+            //    else
+            //    {
+            //        selectionIndex += 20;
+            //    }
+            //}
+            //else if (readKey == ConsoleKey.Enter)
+            //{
 
-            }
+            //}
         }
 
         private void InitializeGame()
         {
             InitializeBlankBuffer();
-            leftPad = 0;
-            rightPad = 44;
-            selectionIndex = ((0 + topPad) * (playfieldWidth + leftPad) + (0 + leftPad));
+            topPad = 3;
+            //leftPad = 24; // if width 88
+            //rightPad = 24; // if width 88
+            leftPad = rightPad = (width - (playfieldWidth*2)) / 2; // padding is the total width - playfield * square size, divided by two.
+            selectionIndex = ((0 + topPad) * (width) + (0 + leftPad));
+            //selectionIndex = 326;
         }
 
         public void DrawToScreen()
@@ -174,46 +169,46 @@ namespace BattleshipGame
         }
         public void WriteToBuffer()
         {
-            int index = 0;
-            int indexTopRight = 0;
-            int indexBottomLeft = 0;
-            int indexBottomRight = 0;
+            int[] index = new int[4];
+
             for (int i = 0; i < bufField.Length; i++)
             {
                 for (int y = 0; y < 20; y++) // 20 = playfield height
                 {
                     for (int x = 0; x < 20; x++) // 20 = playfield width
                     {
-                        if (y != 0)
-                        {
-                            index = ((2*y + topPad) * (width ) + (2*x + leftPad));
-                        }
-                        else
-                        {
-                            index = ((2 * y + topPad) * (width + leftPad) + (2 * x + leftPad));
-                        }
-                        indexTopRight = ((2 * y + topPad) * (width) + (2 * x + leftPad + 1));
-                        indexBottomLeft = ((2 * y + topPad + 1) * (width) + (2 * x + leftPad));
-                        indexBottomRight = ((2 * y + topPad + 1) * (width) + (2 * x + leftPad + 1));
+                        index = ReturnIndexSquare(x, y);
+                        
 
-                        if (index == selectionIndex)
+                        if (index[0] == selectionIndex)
                         {
-                            bufField[index].Attributes = 0x0002;
-                            bufField[indexTopRight].Attributes = 0x0002;
-                            bufField[indexBottomLeft].Attributes = 0x0002;
-                            bufField[indexBottomRight].Attributes = 0x0002;
+                            AssignColor(index, 0x0002);  // Make selection green.
                         }
                         else
                         {
-                            bufField[index].Attributes = 0x0001;
-                            bufField[indexTopRight].Attributes = 0x0001;
-                            bufField[indexBottomLeft].Attributes = 0x0001;
-                            bufField[indexBottomRight].Attributes = 0x0001;
+                            AssignColor(index, 0x0001); // Make non-selected blue
                         }
                     }
                 }
 
             }
+        }
+
+        public int[] ReturnIndexSquare(int x, int y)
+        {
+            int[] indexSquare = new int[4];
+            indexSquare[0] = ((2 * y + topPad) * (width) + (2 * x + leftPad));
+            indexSquare[1] = ((2 * y + topPad) * (width) + (2 * x + leftPad + 1));
+            indexSquare[2] = ((2 * y + topPad + 1) * (width) + (2 * x + leftPad));
+            indexSquare[3] = ((2 * y + topPad + 1) * (width) + (2 * x + leftPad + 1));
+            return indexSquare;
+        }
+        public void AssignColor(int[] index, short color)
+        {
+            bufField[index[0]].Attributes = color;
+            bufField[index[1]].Attributes = color;
+            bufField[index[2]].Attributes = color;
+            bufField[index[3]].Attributes = color;
         }
 
     }
